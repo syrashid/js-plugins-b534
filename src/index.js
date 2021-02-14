2e5a3e3e2041f057e6b3f48318ea8817bde23ade
Sy Rashid <sy.rashid.11@gmail.com>
2021-02-13 21:20:31 -0500

    FIX: Removed example reduce code

--- src/index.js
+++ src/index.js
@@ -1,27 +1,32 @@
 const list = document.querySelector("#results");
 
-const insertMovies = (data) => {
-  data.Search.forEach((result) => {
-    const movie = `<li>
-      <img src="${result.Poster}" alt="" />
-      <p>${result.Title}</p>
-    </li>`;
-    list.insertAdjacentHTML("beforeend", movie);
-  });
-};
+// const insertMovies = (data) => {
+//   data.Search.forEach((result) => {
+//     const movie = `<li>
+//       <img src="${result.Poster}" alt="" />
+//       <p>${result.Title}</p>
+//     </li>`;
+//     list.insertAdjacentHTML("beforeend", movie);
+//   });
+// };
 
-const fetchMovies = (query) => {
-  fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
-    .then((response) => response.json())
-    .then(insertMovies);
-};
+// const fetchMovies = (query) => {
+//   fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
+//     .then((response) => response.json())
+//     .then(insertMovies);
+// };
 
-fetchMovies("harry potter"); // on 1st page load
+// fetchMovies("harry potter"); // on 1st page load
 
-const form = document.querySelector("#search-form");
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  list.innerHTML = "";
-  const input = document.querySelector("#search-input");
-  fetchMovies(input.value);
-});
+// const form = document.querySelector("#search-form");
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   list.innerHTML = "";
+//   const input = document.querySelector("#search-input");
+//   fetchMovies(input.value);
+// });
+
+
+
+
+
