3340107060d97522f63759f687ad1f8c06c7eeab
Sy Rashid <sy.rashid.11@gmail.com>
2021-02-13 23:27:36 -0500

    FEAT: Set up for markdown

--- src/plugins/init_sortable.js
+++ src/plugins/init_sortable.js
@@ -3,7 +3,13 @@ import { Sortable } from "sortablejs";
 const initSortable = () => {
   console.log("FUNCTION CALLED");
   const list = document.querySelector("#results");
-  Sortable.create(list);
+  Sortable.create(list, {
+    ghostClass: "ghost",
+    animation: 150,
+    onEnd: (event) => {
+      alert(`${event.oldIndex} moved to ${event.newIndex}`);
+    },
+  });
 };
 
 export { initSortable };
