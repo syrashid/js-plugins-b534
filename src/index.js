f66b896b1378008e9c78fd0ac66f3c8eeef9a6ec
Sy Rashid <sy.rashid.11@gmail.com>
2021-02-14 18:04:06 -0500

    FEAT: Added sortablejs

--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
-const list = document.querySelector("#results");
-
-const insertMovies = (data) => {
-  data.Search.forEach((result) => {
-    const movie = `<li>
-      <img src="${result.Poster}" alt="" />
-      <p>${result.Title}</p>
-    </li>`;
-    list.insertAdjacentHTML("beforeend", movie);
-  });
-};
-
-const fetchMovies = (query) => {
-  fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
-    .then((response) => response.json())
-    .then(insertMovies);
-};
+import { fetchMovies } from './requests/movies';
+import { initSearchBar } from './components/init_searchbar';
 
 fetchMovies("captain america"); // on 1st page load
 
-const form = document.querySelector("#search-form");
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  list.innerHTML = "";
-  const input = document.querySelector("#search-input");
-  fetchMovies(input.value);
-});
+initSearchBar();
