79c08602c39cd2c21192b41ef2d02fc3c61ea8d8
Sy Rashid <sy.rashid.11@gmail.com>
2021-02-14 18:21:55 -0500

    ARCH: Added markdown package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { fetchMovies } from './requests/movies';
 import { initSearchBar } from './components/init_searchbar';
+import { initSortable } from './plugins/init_sortable';
 
 fetchMovies("captain america"); // on 1st page load
 
 initSearchBar();
+initSortable();
