545905755bcc1583209f9e2a05a262670f7f596a
Sy Rashid <sy.rashid.11@gmail.com>
2021-02-13 23:31:46 -0500

    FEAT: Set up markdown

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { fetchMovies } from "./requests/movies";
 import { initSearchBar } from "./components/init_searchbar";
 import { initSortable } from "./plugins/init_sortable";
+import { initMarkdown } from "./plugins/init_markdown";
 
 fetchMovies("captain america"); // on 1st page load
 
 initSearchBar();
 initSortable();
+initMarkdown();
