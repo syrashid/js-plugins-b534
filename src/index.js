2bd30cd97a8430e69890835a03ecd709ee328be1
Sy Rashid <sy.rashid.11@gmail.com>
2021-02-13 23:56:57 -0500

    FEAT: Added select2

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { fetchMovies } from "./requests/movies";
 import { initSearchBar } from "./components/init_searchbar";
 import { initSortable } from "./plugins/init_sortable";
 import { initMarkdown } from "./plugins/init_markdown";
+import { initSelect2 } from "./plugins/init_select2";
 
 fetchMovies("captain america"); // on 1st page load
 
